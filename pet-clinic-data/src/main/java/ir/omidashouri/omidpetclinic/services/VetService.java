d1d2d67dfcb8dff05c2611e21bf777829425c605
o.ashouri <omidashouri@gmail.com>
2019-09-04 16:12:00 +0430

    refactoring to common crud interface. close #11 : omidashouri : 98.06.13

--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package ir.omidashouri.omidpetclinic.services;
 
 import ir.omidashouri.omidpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
