f09b1907141623cc2ce12fe68f3ff90e4d9be60c
o.ashouri <omidashouri@gmail.com>
2019-09-07 09:52:24 +0430

    Create Vet Speciality Entity, Associate To Vet. 7-24 .close #27 : omidashouri : 98.06.16

---
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Speciality.java
@@ -0,0 +1,9 @@
+package ir.omidashouri.omidpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity{
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Vet.java
@@ -1,5 +1,12 @@
 package ir.omidashouri.omidpetclinic.model;
 
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
 
 }
