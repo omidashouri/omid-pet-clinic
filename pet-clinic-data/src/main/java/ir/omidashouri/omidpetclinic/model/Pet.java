269ffd957ea13e893c9b2ced76aa2a501bab1ded
Omid Ashouri <omidashouri@gmail.com>
2019-09-07 10:58:30 -0700

    Create Visit Entity. 8-21 close #38 : omidashouri : 98.06.16

--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Pet.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -23,6 +25,9 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
 
 }
--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/Visit.java
@@ -2,13 +2,22 @@ package ir.omidashouri.omidpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
 }
