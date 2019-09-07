200a979656aed48924e7e6d009da716da3729d58
o.ashouri <omidashouri@gmail.com>
2019-09-07 12:19:12 +0430

    Create Specialty Map Service Impl. 7-29 .close #31 : omidashouri : 98.06.16

---
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package ir.omidashouri.omidpetclinic.services;
+
+import ir.omidashouri.omidpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+}
---
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,37 @@
+package ir.omidashouri.omidpetclinic.services.map;
+
+import ir.omidashouri.omidpetclinic.model.Speciality;
+import ir.omidashouri.omidpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
