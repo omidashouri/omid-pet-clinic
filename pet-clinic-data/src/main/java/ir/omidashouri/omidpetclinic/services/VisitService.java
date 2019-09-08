9e2d16f3858b2eabc30a89a7ffbfa5f9169089bf
o.ashouri <omidashouri@gmail.com>
2019-09-08 11:08:29 +0430

    Create Visit Map Service. 8-29 close #46 : omidashouri : 98.06.17

---
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package ir.omidashouri.omidpetclinic.services;
+
+import ir.omidashouri.omidpetclinic.model.Vet;
+import ir.omidashouri.omidpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
