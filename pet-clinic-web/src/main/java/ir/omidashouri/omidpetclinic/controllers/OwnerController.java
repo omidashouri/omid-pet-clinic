30a26c28cff03516119a7e142fc41a034bffb54f
Omid Ashouri <omidashouri@gmail.com>
2019-09-09 22:29:08 -0700

    Display Owner Record (HTML) - has error . close #53 . 11-22 : omidashouri : 98.06.19

--- pet-clinic-web/src/main/java/ir/omidashouri/omidpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ir/omidashouri/omidpetclinic/controllers/OwnerController.java
@@ -4,7 +4,10 @@ import ir.omidashouri.omidpetclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -27,4 +30,14 @@ public class OwnerController {
     public String findOwners(){
         return "notimplemented";
     }
+
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerId));
+        return modelAndView;
+
+    }
+
 }
