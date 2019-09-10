9936151974a9a6c051dcfddd617e0e466f3d10cd
Omid Ashouri <omidashouri@gmail.com>
2019-09-10 02:02:40 -0700

    Implement Find Owner . close #54 . 11-25 : omidashouri : 98.06.19

--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package ir.omidashouri.omidpetclinic.repositories;
 import ir.omidashouri.omidpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package ir.omidashouri.omidpetclinic.services;
 
 import ir.omidashouri.omidpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
