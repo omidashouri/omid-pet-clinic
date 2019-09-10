c428a867ee6bbcac9a1e743f045843591c27d0fc
Omid Ashouri <omidashouri@gmail.com>
2019-09-10 10:27:58 -0700

    Add builder pattern to PetType  . #56 . 11-28 : omidashouri : 98.06.19

--- pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ir/omidashouri/omidpetclinic/model/PetType.java
@@ -10,11 +10,16 @@ import javax.persistence.Table;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Data
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Column(name = "name")
     private String name;
