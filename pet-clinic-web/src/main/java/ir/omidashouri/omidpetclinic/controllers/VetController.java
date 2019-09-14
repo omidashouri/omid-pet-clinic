1120bdd9b0c4617ba2a4f6931173b0bf9b2c0547
o.ashouri <omidashouri@gmail.com>
2019-09-14 15:19:35 +0430

    Implement Vets JSON Endpoint. 12-22 close #75 : omidashouri : 98.06.23

--- pet-clinic-web/src/main/java/ir/omidashouri/omidpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ir/omidashouri/omidpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package ir.omidashouri.omidpetclinic.controllers;
 
+import ir.omidashouri.omidpetclinic.model.Vet;
 import ir.omidashouri.omidpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,11 @@ public class VetController {
     public String ListVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
